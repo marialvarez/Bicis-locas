18e959fca27abb8bdd037bb51f85dbe6aca67b7d
Maricela Alvarez <malvarezdrobush@gmail.com>
2017-01-16 11:35:47 -0500

    Terminado

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-  function validateM(_evt){
+//VALIDAR NOMBRE
+function validateM(_evt){
     /*Los campos nombre y apellido sólo deben permitir caracteres de la A-Z*/
         var nombre = document.getElementById("name").value;
-        var apellido = document.getElementById("lastname").value;
         
         var nombreArray = nombre.split("");
         var primeraLetra = nombreArray[0];
@@ -39,31 +39,61 @@
       
 } 
 
+//VALIDAR APELLIDOS
+function validateMA(_evt){
+    /*Los campos nombre y apellido sólo deben permitir caracteres de la A-Z*/
+        var apellido = document.getElementById("lastname").value;
+        
+        var nombreArray = apellido.split("");
+        var primeraLetra = nombreArray[0];
+        var primeraMayus = primeraLetra.toUpperCase();
+        var cortePalabra = false;
+      
+        for(var i=1;i<nombreArray.length;i++){
+            if(cortePalabra)
+            {    
+                primeraMayus += nombreArray[i].toUpperCase();
+                cortePalabra = false;
+            }
+            else
+                primeraMayus+=nombreArray[i];
+            if(nombreArray[i] == " ")
+                cortePalabra = true;     
+        }
+       
+       document.getElementById("lastname").value = primeraMayus; 
+}
+
+
+  function validateTA(_evt){
+    /*Los campos nombre y apellido sólo deben permitir caracteres de la A-Z*/
+        palabra = window.event.keyCode;
+
+        if((palabra>=65 && palabra<=90)||palabra==8||palabra==32){
+            
+        } else {
+            //alert("Este campo solo permite caracteres de la A-Z");
+            _evt.preventDefault();
+        }
+} 
+
+//VALIDAR EMAIL
   function validateEmail(_evt){
     
-      var correos="yahoo hotmail gmail live";
-      var dominio=document.getElementById("email-input").value;
-      var server=dominio[1].split("."); 
-      var res=correos.match(server[0]);
+      var email = document.getElementById("input-email").value;
+      var mensaje = document.getElementById("mensaje3");
       
-       
-        if (dominio.indexOf("@") == -1){
-         alert ("error no tiene arroba");
-         document.getElementById("email-input").focus();
-        } else {
-             if (dominio.indexOf('.') == -1){
-             alert ("falta el punto");
-             document.getElementById("email-input").focus();
-            } else {
-                 if (res==null){
-                 alert ("no pusiste nada");
-                
-        }}}
-       
-    } 
+      if(!/([a-zA-Z0-9(-_.)]+[@][a-zA-Z0-9]+[.][a-zA-Z]+)/g.test(email)){
+        mensaje.innerHTML = "<h5 style='color:#8A2BE2';>Correo Electrónico Inválido</h5>"
+      } else {
+        mensaje.innerHTML = "";
+      }
+      
+} 
 
-  //El campo password debe tener al menos 6 caracteres
-  //El campo password no puede ser igual a "password" ó "123456" ó "098754"    
+
+//El campo password debe tener al menos 6 caracteres
+//El campo password no puede ser igual a "password" ó "123456" ó "098754"    
   function validatePass(_evt){
       
       var pass = document.getElementById("input-password").value;
@@ -77,17 +107,24 @@
       }
         
   }
+    
 
   function validateType(_evt){
       
-      var type = document.querySelect("select").value;
+      var type = document.querySelector("select").value;
       var mensaje = document.getElementById("mensaje2");
       
+      
       if(type == 0){
-          mensaje.innerHTML = "<h5 style='color:#8A2BE2';>No seleccionaste ninguna bicicleta</h5>"
-          return false;
+          mensaje.innerHTML = "<h5 style='color:#8A2BE2';>No seleccionaste ninguna bicicleta</h5>";
       } else {
-          return true;
-      }
+          mensaje.innerHTML = "";
+      }   
   }
+
+//ENVIAR TIPO DE BICICLETA SOLO AL PRESIONAR "REGISTRAR"
+ function validateForm(){
+    validateType();
+ }
+
 
